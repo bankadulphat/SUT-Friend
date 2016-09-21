92761346b1334efb0920e6b4812ca4ea47362040
androidtest <androidtest@gmail.com>
2016-09-21 11:18:15 +0700

    explicit

--- app/src/main/java/android/sut/androidtest/sutfriend/SingUpActivity.java
+++ app/src/main/java/android/sut/androidtest/sutfriend/SingUpActivity.java
@@ -1,14 +1,31 @@
 package android.sut.androidtest.sutfriend;
 
+import android.graphics.drawable.RippleDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SingUpActivity extends AppCompatActivity {
+
+    // explicit ประกาศตัวแปร
+    private EditText nameEditText, addressEditText, phoneEditText, userEditText, passwordEditText;
+    private String nameString, addressString, phoneString, userString, passwordString, genderString,
+    imageString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
-    }
-}
+
+       //bind widget ผูกตัวแปร
+        nameEditText = (EditText) findViewById(R.id.editText );
+        addressEditText = (EditText) findViewById(R.id.editText3);
+        phoneEditText = (EditText) findViewById(R.id.editText4);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+
+    } //main method
+
+}// main class
 
